5c702ccabd3a6e347e3a7ae40014d4c3ebe5bdb5
Gerard Van Halsema <gerardvh@me.com>
2017-09-24 16:52:27 -0500

    Move most of the logic into fish_right_prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,80 @@
 # Removes right prompt
 function fish_right_prompt
+  # Template
+  set -l user_and_host ""
+  set -l current_folder (__parse_current_folder)
+  set -l git_branch_name ""
+  set -l git_dirty ""
+  set -l git_arrows ""
+  set -l command_duration ""
+  set -l prompt ""
+
+  # Check if user is in an SSH session
+  if [ "$SSH_CONNECTION" != "" ]
+    set -l host (hostname -s)
+    set -l user (whoami)
+
+    if [ "$user" = "root" ]
+      set user "$pure_root_color$user"
+    else
+      set user "$pure_username_color$user"
+    end
+
+    # Format user and host part of prompt
+    set user_and_host "$user$pure_color_gray@$pure_host_color$host$pure_color_normal "
+  end
+
+  if test $pure_user_host_location -eq 1
+    set prompt $prompt $user_and_host
+  end
+
+  # Format current folder on prompt output
+  set prompt $prompt "$pure_color_blue$current_folder$pure_color_normal "
+
+  # Exit with code 1 if git is not available
+  if not which git >/dev/null
+    return 1
+  end
+
+  # Check if is on a Git repository
+  set -l is_git_repository (command git rev-parse --is-inside-work-tree ^/dev/null)
+
+  if test -n "$is_git_repository"
+    set git_branch_name (__parse_git_branch)
+
+    # Check if there are files to commit
+    set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
+
+    if test -n "$is_git_dirty"
+      set git_dirty $pure_symbol_git_dirty
+    end
+
+    # Check if there is an upstream configured
+    command git rev-parse --abbrev-ref '@{upstream}' >/dev/null ^&1; and set -l has_upstream
+    if set -q has_upstream
+      set -l git_status (command git rev-list --left-right --count 'HEAD...@{upstream}' | sed "s/[[:blank:]]/ /" ^/dev/null)
+
+      # Resolve Git arrows by treating `git_status` as an array
+      set -l git_arrow_left (command echo $git_status | cut -c 1 ^/dev/null)
+      set -l git_arrow_right (command echo $git_status | cut -c 3 ^/dev/null)
+
+    # If arrow is not "0", it means it's dirty
+      if test $git_arrow_left != 0
+        set git_arrows " $pure_symbol_git_up_arrow"
+      end
+
+      if test $git_arrow_right != 0
+        set git_arrows " $git_arrows$pure_symbol_git_down_arrow"
+      end
+    end
+
+    # Format Git prompt output
+    set prompt $prompt "$pure_color_gray$git_branch_name$git_dirty$pure_color_normal$pure_color_cyan$git_arrows$pure_color_normal "
+  end
+
+  if test $pure_user_host_location -ne 1
+    set prompt $prompt $user_and_host
+  end
+
+  echo -e -s $prompt
 end
