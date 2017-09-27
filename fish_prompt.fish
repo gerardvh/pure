b59f83c872f5ebbc3ed6a1b1ce6cdc24c38bb7a6
Gerard Van Halsema <gerardvh@me.com>
2017-09-27 09:46:35 -0500

    Change git prompt to use built-in fish git prompt function

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -50,7 +50,7 @@ function fish_prompt
   # Template
 
   set -l user_and_host ""
-  set -l current_folder (__parse_current_folder)
+  set -l current_folder (prompt_pwd)
   set -l git_branch_name ""
   set -l git_dirty ""
   set -l git_arrows ""
@@ -99,36 +99,37 @@ function fish_prompt
   set -l is_git_repository (command git rev-parse --is-inside-work-tree ^/dev/null)
 
   if test -n "$is_git_repository"
-    set git_branch_name (__parse_git_branch)
+    set prompt $prompt (__fish_git_prompt)" "
+    # set git_branch_name (__parse_git_branch)
 
-    # Check if there are files to commit
-    set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
+    # # Check if there are files to commit
+    # set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
 
-    if test -n "$is_git_dirty"
-      set git_dirty $pure_symbol_git_dirty
-    end
+    # if test -n "$is_git_dirty"
+    #   set git_dirty $pure_symbol_git_dirty
+    # end
 
-    # Check if there is an upstream configured
-    command git rev-parse --abbrev-ref '@{upstream}' >/dev/null ^&1; and set -l has_upstream
-    if set -q has_upstream
-      set -l git_status (command git rev-list --left-right --count 'HEAD...@{upstream}' | sed "s/[[:blank:]]/ /" ^/dev/null)
+    # # Check if there is an upstream configured
+    # command git rev-parse --abbrev-ref '@{upstream}' >/dev/null ^&1; and set -l has_upstream
+    # if set -q has_upstream
+    #   set -l git_status (command git rev-list --left-right --count 'HEAD...@{upstream}' | sed "s/[[:blank:]]/ /" ^/dev/null)
 
-      # Resolve Git arrows by treating `git_status` as an array
-      set -l git_arrow_left (command echo $git_status | cut -c 1 ^/dev/null)
-      set -l git_arrow_right (command echo $git_status | cut -c 3 ^/dev/null)
+    #   # Resolve Git arrows by treating `git_status` as an array
+    #   set -l git_arrow_left (command echo $git_status | cut -c 1 ^/dev/null)
+    #   set -l git_arrow_right (command echo $git_status | cut -c 3 ^/dev/null)
 
-    # If arrow is not "0", it means it's dirty
-      if test $git_arrow_left != 0
-        set git_arrows " $pure_symbol_git_up_arrow"
-      end
+    # # If arrow is not "0", it means it's dirty
+    #   if test $git_arrow_left != 0
+    #     set git_arrows " $pure_symbol_git_up_arrow"
+    #   end
 
-      if test $git_arrow_right != 0
-        set git_arrows " $git_arrows$pure_symbol_git_down_arrow"
-      end
-    end
+    #   if test $git_arrow_right != 0
+    #     set git_arrows " $git_arrows$pure_symbol_git_down_arrow"
+    #   end
+    # end
 
-    # Format Git prompt output
-    set prompt $prompt "$pure_color_gray$git_branch_name$git_dirty$pure_color_normal$pure_color_cyan$git_arrows$pure_color_normal "
+    # # Format Git prompt output
+    # set prompt $prompt "$pure_color_gray$git_branch_name$git_dirty$pure_color_normal$pure_color_cyan$git_arrows$pure_color_normal "
   end
 
   if test $pure_user_host_location -ne 1
